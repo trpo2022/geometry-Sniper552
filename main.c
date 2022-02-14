05acb819ddd88d981c54876cb12fee956f64bf34
Осипов Владислав <vladosp55@mail.ru>
2022-02-14 17:37:47 +0700

    Added calculation of area and circumference

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
-int main()
-{
-    printf("Hello, World!");
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+float okr(float r){
+    float a,b;
+    b=2*M_PI*r;
+    a=M_PI*r*r;
+    printf("Длина окружности - %f\n",b);
+    printf("Площадь окружности - %f",a);
+}
+int main(){
+    setlocale(0,"rus");
+    float r;
+    printf("Введите радиус окружности:\n");
+    scanf("%f",&r);
+    okr(r);
     return 0;
 }
+

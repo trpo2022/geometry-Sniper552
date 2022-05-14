0ec3fecc47c7a0bd3fc757c49db0ddb96677b3ce
Осипов Владислав <vladosp55@mail.ru>
2022-05-14 15:45:16 +0700

    Update test

--- test/test.cpp
+++ test/test.cpp
@@ -1,30 +1,32 @@
 #include <ctest.h>
 #include <libmain/check.h>
 
+
 CTEST(suite, test1_true)
 {
-    int result = ProcessGeomFile("test/testfile/test1.txt");
+   char filename[]="test/testfile/test1.txt";
+    int result = circles(filename);
 
     ASSERT_FALSE(result);
 }
 
 CTEST(suite, test2_true)
 {
-    int result = ProcessGeomFile("test/testfile/test2.txt");
+    int result = circles("test/testfile/test2.txt");
 
     ASSERT_FALSE(result);
 }
 
 CTEST(suite, test3_true)
 {
-    int result = ProcessGeomFile("test/testfile/test3.txt");
+    int result = circles("test/testfile/test3.txt");
 
     ASSERT_FALSE(result);
 }
 
 CTEST(suite, test4_true)
 {
-    int result = ProcessGeomFile("test/testfile/test4.txt");
+    int result = circles("test/testfile/test4.txt");
 
     ASSERT_FALSE(result);
 }
